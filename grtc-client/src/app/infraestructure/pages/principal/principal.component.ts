2158fdcdabb0ee3833784c012d94ebec9256ee9c
neto <tu.email@ejemplo.com>
2024-11-26 17:13:56 -0500

    footer finalizado y puesto en la pagina principal

--- grtc-client/src/app/infraestructure/pages/principal/principal.component.ts
+++ grtc-client/src/app/infraestructure/pages/principal/principal.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavegadorComponent } from '../../shared/components/navegador/navegador.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [NavegadorComponent],
+  imports: [NavegadorComponent,FooterComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
